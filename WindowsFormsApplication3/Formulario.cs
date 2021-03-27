d701d3fdffc3b0d556ab1933b8135cd32891763e
Edy Esquivel <edy.esquivel@outlook.com>
2021-03-27 00:52:35 -0600

    Carga Laboratorio

---
+++ WindowsFormsApplication3/Formulario.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+
+        ArbolBinarioBusqueda b = new ArbolBinarioBusqueda();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Tarea nuevaTarea = new Tarea(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+            Nodo n = new Nodo(nuevaTarea);
+            b.insertar(n);
+            MessageBox.Show("Agregado Exitosamente");
+            textBox1.Text="";
+            textBox2.Text="";
+            textBox3.Text="";
+            textBox4.Text = "";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String a=ArbolBinario.preorden(b.raizArbol());
+            agregartexto(a);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String a = ArbolBinario.postorden(b.raizArbol());
+            agregartexto(a);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            String a = ArbolBinario.inorden(b.raizArbol());
+            agregartexto(a);
+        }
+
+        public void agregartexto(string full) {
+
+            listBox1.Items.Clear();
+            string[] pasando = full.Split(new char[] { '-' });
+            List<string> lines = new List<string>();
+            foreach(string s in pasando){
+                listBox1.Items.Add(s);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Nodo resultado= b.buscar(new Tarea(textBox5.Text));
+            if (resultado == null)
+                MessageBox.Show("No se encontro el resultado");
+            else {
+                Tarea t = (Tarea)resultado.valorNodo();
+
+                MessageBox.Show("Encontrado: Nombre:"+t.nombre()+" Duracion: "+t.duracion()+"  Porcentaje: "+t.porcentaje()+" Programador: "+t.programador());
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Nodo resultado= b.buscar(new Tarea(textBox5.Text));
+            b.eliminar(resultado);
+            MessageBox.Show("Elminacion exitosa");
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApplication3/Tarea.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication3
+{
+    public class Tarea:Comparador
+    {
+        protected string Nombre;
+        protected string Duracion;
+        protected string Porcentaje;
+        protected string Programador;
+
+        public Tarea() {
+            Nombre = "";
+        
+        }
+
+        public Tarea(string nombre)
+        {
+            Nombre = nombre;
+
+        }
+
+
+        public Tarea(string nombre, string duracion, string porcentaje, string programador)
+        {
+            Nombre = nombre;
+            Duracion = duracion;
+            Porcentaje = porcentaje;
+            Programador = programador;
+
+
+        }
+        public string nombre() {
+
+            return Nombre;
+        }
+        public void nombre(string nombre)
+        {
+
+            Nombre=nombre;
+        }
+        public string duracion()
+        {
+
+            return Duracion;
+        }
+        public void duracion(string duracion)
+        {
+
+            Duracion = duracion;
+        }
+        public string porcentaje()
+        {
+
+            return Porcentaje;
+        }
+        public void porcentaje(string porcentaje)
+        {
+
+            Porcentaje = porcentaje;
+        }
+        public string programador()
+        {
+
+            return Programador;
+        }
+        public void programador(string programador)
+        {
+
+            Programador = programador;
+        }
+       public bool igualQue(Object q){
+            
+            if (this.Nombre.CompareTo(((Tarea)q).nombre())==0 ) {
+                return true;
+            }
+            
+            return false;
+        }
+       public bool menorQue(Object q) {
+            if (this.Nombre.CompareTo(((Tarea)q).nombre()) < 0)
+            {
+                return true;
+            }
+
+            return false;
+        
+        }
+       public bool menorIgualQue(Object q) {
+            if (this.Nombre.CompareTo(((Tarea)q).nombre()) <= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+       public bool mayorQue(Object q) {
+            if (this.Nombre.CompareTo(((Tarea)q).nombre()) > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+       public bool mayorIgualQue(Object q) {
+            if (this.Nombre.CompareTo(((Tarea)q).nombre()) >= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
